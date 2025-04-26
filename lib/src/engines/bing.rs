1f431d1f607671ac8d28658cf4510b82a34a5d5d
--get <ashwinvinodsa@gmail.com>
2025-04-26 07:32:56 +0530

    fix(bing): Remove "a1" from the start of the base64 encoded url.
    !IMPORTANT: This commit was made while refactoring engine handler, this commit
    will not compile for now.

--- lib/src/engines/bing.rs
+++ lib/src/engines/bing.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    collections::HashMap,
+    sync::{Arc, LazyLock},
+};
 
 use regex::Regex;
 
@@ -10,10 +13,32 @@ use crate::{
     errors::EngineErrorType, network::NetworkHandler, Relavancy, SafeSearchLevel, SearchResult,
 };
 
-use super::{parse_generic_results, Engine};
+use super::Engine;
 
 const COOKIE_PARAMS: &str =
     "_EDGE_V=1;SRCHD=AF=NOFORM;_Rwho=u=d;bngps=s=0;_UR=QS=0&TQS=0;_UR=QS=0&TQS=0;";
+
+static NO_RESULTS_SELECTOR: LazyLock<Selector> =
+    LazyLock::new(|| Selector::parse(".b_results").unwrap());
+static TEXT_RESULTS_SELECTOR: LazyLock<Selector> =
+    LazyLock::new(|| Selector::parse(".b_algo").unwrap());
+static TEXT_RESULT_URL_SELECTOR: LazyLock<Selector> =
+    LazyLock::new(|| Selector::parse(".b_tpcn a.tilk").unwrap());
+static TEXT_RESULT_TITLE_SELECTOR: LazyLock<Selector> =
+    LazyLock::new(|| Selector::parse("h2 a").unwrap());
+static TEXT_RESULT_DESC_SELECTOR: LazyLock<Vec<Selector>> = LazyLock::new(|| {
+    [
+        Selector::parse(".b_caption p").unwrap(),
+        Selector::parse(".b_lineclamp3 p").unwrap(),
+    ]
+    .to_vec()
+});
+
+static RE_SPAN: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r#"<span.*?>.*?(?:</span>&nbsp;·|</span>)"#).unwrap());
+static RE_STRONG: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r#"(<strong>|</strong>)"#).unwrap());
+
 /// Get the actual url of the website by decoding query param.
 fn get_og_url(url: &str) -> Result<String, EngineErrorType> {
     Url::parse(url)
@@ -21,7 +46,7 @@ fn get_og_url(url: &str) -> Result<String, EngineErrorType> {
         .query_pairs()
         .find_map(|(k, v)| {
             k.eq("u").then_some(
-                base64_url::decode(v.as_bytes())
+                base64_url::decode(v.trim_start_matches("a1").as_bytes())
                     .ok()
                     .and_then(|val| String::from_utf8(val).ok()),
             )
@@ -29,35 +54,9 @@ fn get_og_url(url: &str) -> Result<String, EngineErrorType> {
         .flatten()
         .ok_or(EngineErrorType::ParseFailed)
 }
+
 #[derive(Debug)]
-pub struct Bing {
-    no_results_selector: Selector,
-    text_results_selector: Selector,
-    text_result_url_selector: Selector,
-    text_result_title_selector: Selector,
-    text_result_desc_selector: Vec<Selector>,
-    re_strong: Regex,
-    re_span: Regex,
-}
-
-impl Bing {
-    pub fn new() -> Arc<Box<dyn Engine>> {
-        Arc::new(Box::new(Self {
-            no_results_selector: Selector::parse(".b_results").unwrap(),
-            text_results_selector: Selector::parse(".b_algo").unwrap(),
-            text_result_url_selector: Selector::parse(".b_tpcn a.tilk").unwrap(),
-            text_result_title_selector: Selector::parse("h2 a").unwrap(),
-            text_result_desc_selector: [
-                Selector::parse(".b_caption p").unwrap(),
-                Selector::parse(".b_lineclamp3 p").unwrap(),
-            ]
-            .to_vec(),
-
-            re_span: Regex::new(r#"<span.*?>.*?(?:</span>&nbsp;·|</span>)"#).unwrap(),
-            re_strong: Regex::new(r#"(<strong>|</strong>)"#).unwrap(),
-        }))
-    }
-}
+pub struct Bing;
 
 #[async_trait::async_trait]
 impl Engine for Bing {
@@ -92,7 +91,7 @@ impl Engine for Bing {
         let page = qclient.get_data(&url, headers, false).await?;
         let page = Html::parse_document(&page);
 
-        if let Some(no_result_msg) = page.select(&self.no_results_selector).nth(0) {
+        if let Some(no_result_msg) = page.select(&NO_RESULTS_SELECTOR).nth(0) {
             if no_result_msg
                 .value()
                 .attr("class")
@@ -103,29 +102,39 @@ impl Engine for Bing {
             }
         }
 
-        let results = parse_generic_results(&page, &self.text_results_selector, |result| {
-            let title = result.select(&self.text_result_title_selector).next();
-            let url = result.select(&self.text_result_url_selector).next();
-            let desc = &self
-                .text_result_desc_selector
+        let mut parsed_results: Vec<SearchResult> = Vec::new();
+
+        for result in page.select(&TEXT_RESULTS_SELECTOR) {
+            let title = result.select(&TEXT_RESULT_TITLE_SELECTOR).next();
+            let url = result.select(&TEXT_RESULT_URL_SELECTOR).next();
+            let desc = &TEXT_RESULT_DESC_SELECTOR
                 .iter()
                 .find_map(|slctr| result.select(slctr).next());
 
             if let (Some(title), Some(url), Some(desc)) = (title, url, desc) {
-                SearchResult::new(
-                    &get_og_url(&url.value().attr("href").unwrap()).unwrap(),
-                    &self.re_strong.replace_all(title.inner_html().trim(), ""),
-                    &self.re_span.replace_all(desc.inner_html().trim(), ""),
-                    "Bing",
-                )
-                .ok()
-            } else {
-                None
-            }
-        })
-        .map_err(|_| EngineErrorType::ParseFailed)?;
+                let p_url = if let Some(url) = url.value().attr("href") {
+                    if url.starts_with("https://www.bing.com/ck/a?") {
+                        &get_og_url(url).unwrap()
+                    } else {
+                        url
+                    }
+                } else {
+                    tracing::warn!("Could not parse search result url!");
+                    continue;
+                };
 
-        tracing::trace!("Bing returned {} results.", results.len());
-        Ok(results)
+                parsed_results.push(SearchResult::new(
+                    &p_url,
+                    &RE_STRONG.replace_all(title.inner_html().trim(), ""),
+                    &RE_SPAN.replace_all(desc.inner_html().trim(), ""),
+                    "Bing",
+                )?);
+            } else {
+                continue;
+            }
+        }
+
+        tracing::trace!("Bing returned {} results.", parsed_results.len());
+        Ok(parsed_results)
     }
 }
